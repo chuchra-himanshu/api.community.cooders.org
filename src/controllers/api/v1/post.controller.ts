e88677177e7fa1931e73489031944fa38ceae612
Himanshu Chuchra <code@himanshuchuchra.com>
2025-07-14 18:07:11 +0530

    Write Post Controllers - GET and DELETE Post

--- src/controllers/api/v1/post.controller.ts
+++ src/controllers/api/v1/post.controller.ts
@@ -1,11 +1,11 @@
 // Import Section
 import { type Response, type Request } from "express";
-import { APIResponse, asyncHandler } from "../../../handlers";
+import { APIError, APIResponse, asyncHandler } from "../../../handlers";
 import { Post } from "../../../models";
 
 // Controller End-Points
 const createPost = asyncHandler(async (req: Request, res: Response) => {
-  const user = "";
+  const { user_id } = req.params;
   const {
     description,
     snippets,
@@ -26,7 +26,7 @@ const createPost = asyncHandler(async (req: Request, res: Response) => {
   } = req.body;
 
   const postPayload = {
-    user,
+    user: user_id,
     description,
     snippets: snippets || [],
     tags: tags || [],
@@ -53,13 +53,58 @@ const createPost = asyncHandler(async (req: Request, res: Response) => {
     .json(APIResponse.send(201, "Post created successfully"));
 });
 
-const getPosts = asyncHandler(async (req: Request, res: Response) => {});
+const getPosts = asyncHandler(async (req: Request, res: Response) => {
+  const { user_id } = req.params;
 
-const getPost = asyncHandler(async (req: Request, res: Response) => {});
+  const posts = await Post.find({ user: user_id, status: { $ne: "DELETED" } })
+    .sort({ createdAt: -1 })
+    .lean();
+
+  return res
+    .status(200)
+    .json(APIResponse.send(200, "Posts fetched successfully", posts));
+});
+
+const getPost = asyncHandler(async (req: Request, res: Response) => {
+  const { postId } = req.params;
+
+  const post = await Post.findOne({
+    _id: postId,
+    status: { $ne: "DELETED" },
+  }).lean();
+  if (!post) {
+    return res.status(404).json(APIError.send(404, "Post not found!"));
+  }
+
+  return res
+    .status(200)
+    .json(APIResponse.send(200, "Post fetched successfully", post));
+});
 
 const updatePost = asyncHandler(async (req: Request, res: Response) => {});
 
-const deletePost = asyncHandler(async (req: Request, res: Response) => {});
+const deletePost = asyncHandler(async (req: Request, res: Response) => {
+  const { post_id, user_id } = req.params;
+
+  const post = await Post.findById(post_id);
+
+  if (!post || post.status === "DELETED") {
+    return res.status(404).json(APIError.send(404, "Post not found!"));
+  }
+
+  if (post.user.toString() !== user_id?.toString()) {
+    return res
+      .status(403)
+      .json(APIError.send(404, "You are not authorized to delete this post"));
+  }
+
+  post.status = "DELETED";
+  await post.save();
+
+  return res
+    .status(200)
+    .json(APIResponse.send(200, "Post deleted successfully"));
+});
 
 const reactionOnPost = asyncHandler(async (req: Request, res: Response) => {});
 
